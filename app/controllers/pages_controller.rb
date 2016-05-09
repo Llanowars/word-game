13c15ebc35d68fae9a5ff934910c337bc39285b9
Julien Da Silva <julien.da.silva@hotmail.com>
2016-05-09 22:44:43 +0200

    grid & word pushed to the view

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+require 'game'
+
+class PagesController < ApplicationController
+  def game
+  @grid = generate_grid(9)
+  @start_time = (Time.now)
+  end
+
+  def score
+  @attempt = params[:attempt]
+  @grid = params[:grid]
+  # @start_time = Date.parse params[:time]
+  # @end_time = Time.now
+  end
+
+  def generate_grid(grid_size)
+  Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+end
