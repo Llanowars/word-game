ff06abd30d198ea4f51bdb68ad8a4edf12d46d67
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-25 23:20:02 +0100

    oulala

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,20 @@
 require 'game'
 
 class PagesController < ApplicationController
+  
   def game
-  @grid = generate_grid(9)
-  @start_time = (Time.now)
+    @grid = generate_grid(9)
+    @start_time = (Time.now)
   end
 
   def score
-  @attempt = params[:attempt]
-  @grid = params[:grid]
+    @attempt = params[:attempt]
+    @grid = params[:grid]
   # @start_time = Date.parse params[:time]
   # @end_time = Time.now
   end
 
   def generate_grid(grid_size)
-  Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+    Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
   end
-
 end
